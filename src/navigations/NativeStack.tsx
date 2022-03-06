ddf1b1ed40c98ccbf2513f07b12bafe85aec005b
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-06 22:37:02 +0900

    feat: Create native stack navigation

--- src/navigations/NativeStack.tsx
+++ src/navigations/NativeStack.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Chart from '../screen/Chart';
 import Home from '../screen/Home';
-import Map from '../screen/Map';
 
 const Stack = createNativeStackNavigator();
 
-export default function MyStack() {
+function NativeStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Map" component={Map} />
-      <Stack.Screen name="Chart" component={Chart} />
     </Stack.Navigator>
   );
 }
+
+export default NativeStack;
