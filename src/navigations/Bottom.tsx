a7b11b40ac0819612828fb6fd712caa3a4282197
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-06 22:37:22 +0900

    feat: darkMode style setting

--- src/navigations/Bottom.tsx
+++ src/navigations/Bottom.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Chart from '../screen/Chart';
 import Map from '../screen/Map';
+import Setting from '../screen/Setting';
+import {useSelector} from 'react-redux';
+import {RootState} from '../redux/reducer';
 
 const Tab = createBottomTabNavigator();
 
-export default function MyTabs() {
+function Bottom() {
+  const {isDark} = useSelector((state: RootState) => state.theme);
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: isDark ? 'black' : 'white',
+        },
+        headerTitleStyle: {
+          color: isDark ? 'white' : 'black',
+        },
+        tabBarStyle: {
+          backgroundColor: isDark ? 'black' : 'white',
+        },
+      }}>
       <Tab.Screen name="Map" component={Map} />
       <Tab.Screen name="Chart" component={Chart} />
+      <Tab.Screen name="Setting" component={Setting} />
     </Tab.Navigator>
   );
 }
+
+export default Bottom;
