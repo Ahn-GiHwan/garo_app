2503b4ecd056da359856086246432d887ebf11bb
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-09 23:06:35 +0900

    feat: HeaderLeft component of react-navigation

---
+++ src/components/LeftHeader.tsx
@@ -0,0 +1,28 @@
+import React, {useCallback} from 'react';
+import styled from 'styled-components/native';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {useSelector} from 'react-redux';
+import {NativeStackParamList} from '../navigations/NativeStack';
+import {RootState} from '../redux/reducer';
+import Icon from './Icon';
+
+const Container = styled.TouchableOpacity`
+  justify-content: center;
+`;
+
+function LeftHeader() {
+  const {isDark} = useSelector((state: RootState) => state.theme);
+  const navigation = useNavigation<NavigationProp<NativeStackParamList>>();
+
+  const onGoHome = useCallback(() => {
+    navigation.navigate('Home');
+  }, [navigation]);
+
+  return (
+    <Container onPress={onGoHome}>
+      <Icon name="home-outline" color={isDark ? 'white' : 'black'} size={20} />
+    </Container>
+  );
+}
+
+export default LeftHeader;
