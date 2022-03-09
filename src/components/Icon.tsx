ae5ffab3861037af941de2fc83f05140fbc37413
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-09 23:47:10 +0900

    fix: color가 없을 시 조건에 '()'추가

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,7 +5,7 @@ import {RootState} from '../redux/reducer';
 
 function Icon({name, color, size}: {name: string; color?: string; size: number}) {
   const {isDark} = useSelector((state: RootState) => state.theme);
-  return <Ionicons name={name} color={color || isDark ? 'white' : 'black'} size={size} />;
+  return <Ionicons name={name} color={color || (isDark ? 'white' : 'black')} size={size} />;
 }
 
 export default Icon;
