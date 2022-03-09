9d3c1662b19ad5447811d6a008d2f70ae1c4a5c3
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-09 23:10:13 +0900

    feat: Create native-stack-navigation

--- src/navigations/NativeStack.tsx
+++ src/navigations/NativeStack.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screen/Home';
+import Bottom from './Bottom';
+
+export type NativeStackParamList = {
+  Home: undefined;
+  Bottom: {name: string};
+};
 
 const Stack = createNativeStackNavigator();
 
 function NativeStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Bottom" component={Bottom} />
     </Stack.Navigator>
   );
 }
