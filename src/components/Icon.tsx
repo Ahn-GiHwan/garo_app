d0eb9dc39a2ae5b52fdf2a1758384d8bc726b82b
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-09 23:31:26 +0900

    fix: Icon component에 color props가 없으면 현재 테마모드에 따라 color가 지정

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {useSelector} from 'react-redux';
+import {RootState} from '../redux/reducer';
 
-function Icon({name, color, size}: {name: string; color: string; size: number}) {
-  return <Ionicons name={name} color={color} size={size} />;
+function Icon({name, color, size}: {name: string; color?: string; size: number}) {
+  const {isDark} = useSelector((state: RootState) => state.theme);
+  return <Ionicons name={name} color={color || isDark ? 'white' : 'black'} size={size} />;
 }
 
 export default Icon;
