aee88d1267d7cbbfd2debe8ac695e98f1df6d0f5
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-09 23:05:44 +0900

    fix: Remove '

--- styled-components.d.ts
+++ styled-components.d.ts
@@ -3,15 +3,15 @@ import 'styled-components';
 // and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
-    bg: 'string';
-    color: 'string';
+    bg: string;
+    color: string;
   }
   export interface darkTheme {
-    bg: 'string';
-    color: 'string';
+    bg: string;
+    color: string;
   }
   export interface lightTheme {
-    bg: 'string';
-    color: 'string';
+    bg: string;
+    color: string;
   }
 }
