599afbf554049a29690f546c0b2b14da3a15b7ac
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-10 11:47:08 +0900

    feat: Add ChartBg

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,9 +1,11 @@
 export const lightTheme = {
   bg: 'white',
+  chartBg: 'white',
   color: 'black',
 };
 
 export const darkTheme = {
   bg: '#262422',
+  chartBg: '#b9b5b5',
   color: 'white',
 };
