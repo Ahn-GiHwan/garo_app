8895c8a20e533a6b4a8079035be35a2202ab3c8a
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-10 17:52:23 +0900

    feat: 리스트에서 지역 클릭 시 그 지역에 맞게 지도의 center를 바꿔줄 수 있게 geocode를 알려주는 함수 추가 및 NaverMap에 props로 결과값 전달 및 예외처리

--- src/screen/Map.tsx
+++ src/screen/Map.tsx
@@ -5,8 +5,9 @@ import EachLocList from '../components/EachLocList';
 import Icon from '../components/Icon';
 import {View} from '../style/common';
 import {useQuery} from 'react-query';
-import {getBoroughNameFetch} from '../../api';
 import Loading from '../components/Loading';
+import {getBoroughsFetch, getBoroughsGeocodeFetch} from '../apis/map';
+import {Alert} from 'react-native';
 
 const Container = styled(View)`
   flex: 1;
@@ -30,21 +31,38 @@ const ListButton = styled.Pressable`
 `;
 
 function Map() {
-  const {isLoading, data} = useQuery('getBoroughName', getBoroughNameFetch);
+  const {isLoading, data} = useQuery('getChartBoroughs', getBoroughsFetch);
   const [isList, setIsList] = useState(false);
+  const [geocode, setGeocode] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const onIsListToggle = useCallback((): void => {
     setIsList(prev => !prev);
   }, []);
 
-  const renderItem = useCallback(({item: {id, borough}}) => <EachLocList id={id} borough={borough} />, []);
+  const onClickBorough = useCallback(async borough => {
+    setLoading(true);
+    try {
+      const result = await getBoroughsGeocodeFetch(borough);
+      setGeocode(result);
+    } catch (error) {
+      Alert.alert('에러', 'geocode불러오기 에러');
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
-  if (isLoading) {
+  const renderItem = useCallback(
+    ({item: {id, borough}}) => <EachLocList id={id} borough={borough} onClickBorough={onClickBorough} />,
+    [onClickBorough],
+  );
+
+  if (isLoading || loading) {
     return <Loading />;
   } else {
     return (
       <Container>
-        <NaverMap isList={isList} />
+        <NaverMap isList={isList} geocode={geocode} />
         <FlatList data={data} keyExtractor={(item: any) => item.id} renderItem={renderItem} />
         <ListButton onPress={onIsListToggle}>
           <Icon name={isList ? 'close' : 'list'} size={25} />
