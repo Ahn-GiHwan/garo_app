ee0a786bd779305bd9f68b5b0c4fe7863e56de75
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-10 18:05:47 +0900

    feat: blur props를 추가하여 새로운 디자인의 로딩 애니메이션 기능 추가

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import {ActivityIndicator} from 'react-native';
+import {ActivityIndicator, StyleSheet} from 'react-native';
 import {useSelector} from 'react-redux';
 import styled from 'styled-components/native';
 import {RootState} from '../redux/reducer';
 import {View} from '../style/common';
 
 const LoadingContainer = styled(View)`
+  ${({blur}) => blur && StyleSheet.absoluteFill};
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: ${({blur, theme}) => (blur ? 'rgba(1, 1, 1, 0.5)' : theme.bg)};
 `;
 
-function Loading() {
+function Loading({blur}: {blur?: boolean}) {
   const {isDark} = useSelector((state: RootState) => state.theme);
   return (
-    <LoadingContainer>
+    <LoadingContainer blur={blur}>
       <ActivityIndicator color={isDark ? 'white' : 'black'} size="large" />
     </LoadingContainer>
   );
