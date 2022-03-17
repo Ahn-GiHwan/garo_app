619b3e6ba95b3c736c75fd14e1f0c2e6eabc190a
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-18 01:28:31 +0900

    feat: Ge current location

--- src/screen/Map.tsx
+++ src/screen/Map.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import styled from 'styled-components/native';
 import NaverMap from '../components/NaverMap';
 import EachLocList from '../components/EachLocList';
@@ -8,6 +8,7 @@ import {useQuery} from 'react-query';
 import Loading from '../components/Loading';
 import {getBoroughsFetch, getBoroughsGeocodeFetch} from '../apis/map';
 import {Alert} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
 const Container = styled(View)`
   flex: 1;
@@ -30,11 +31,32 @@ const ListButton = styled.Pressable`
   background-color: ${({theme}) => theme.bg};
 `;
 
+export type Igeocode = {
+  latitude: number;
+  longitude: number;
+};
+
 function Map() {
   const {isLoading, data} = useQuery('getChartBoroughs', getBoroughsFetch);
   const [isList, setIsList] = useState(false);
-  const [geocode, setGeocode] = useState(null);
+  const [geocode, setGeocode] = useState<Igeocode>({latitude: 0, longitude: 0});
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      info => {
+        setGeocode({
+          latitude: info.coords.latitude,
+          longitude: info.coords.longitude,
+        });
+      },
+      console.error,
+      {
+        enableHighAccuracy: false,
+        timeout: 20000,
+      },
+    );
+  }, []);
 
   const onIsListToggle = useCallback((): void => {
     setIsList(prev => !prev);
